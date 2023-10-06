3a95aa866e90516c53b9b67f964c038a4e39e763
reeuze <adittitik@gmail.com>
2023-10-06 23:38:49 +0700

    Commit 2 (Set up model)

--- Main.py
+++ Main.py
@@ -1,25 +1,27 @@
 class Basic:
     def __init__(self):
         self.File = open("Code.txt", "r")
-        self.Scan = ""
-        self.Read = []
-    def Inisialitation(self, scan, file):
-        if scan in file:
+        self.Variabel = []
+    def Inisialitation(self, scan):
+        if '=' in scan:
             return True
         else:
             return False
-    # def Input(self):
-    # def Output(self):
-    # def Condition(self):
-    # def Looping(self):
-    # def Stack(self):
-    # def Function(self):
+    # def Input(self): 
+    # def Output(self): 
+    # def Condition(self): 
+    # def Looping(self): 
+    # def Stack(self): 
+    # def Function(self): 
     def Execution(self):
-        self.Scan = self.File.readline().strip()
-        while self.Scan:
-            # Check line
+        self.scan = self.File.readline().strip()
+        while self.scan:
+            self.temp = []
+            # Check Function
+            if self.Inisialitation(self.scan) is True:
+                continue
 
-            self.Scan = self.File.readline().strip()
+            self.scan = self.File.readline().strip()
         self.File.close()
 
 a = Basic()
