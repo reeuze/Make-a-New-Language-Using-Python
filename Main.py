c1724f6f288cc2329bd8184e80acc1c8c37b9cb8
reeuze <adittitik@gmail.com>
2023-10-08 19:59:33 +0700

    Commit 4 (Failed)

--- Main.py
+++ Main.py
@@ -3,35 +3,44 @@ class Basic:
         self.File = open("Code.txt", "r")
         self.Variabel = []
     def Check_line(self, scan):
+        # print(scan)
         if '=' in scan:
+            print(1)
             return 1
         elif 'input' in scan:
             return 2
         elif 'print' in scan:
+            print(3)
             return 3
         elif ('if' in scan) or ('elif' in scan) or ('else' in scan):
             return 4
         elif ('for' in scan):
             return 5
-    def Inisialitation(self, scan):
-        if '=' in scan:
-            return True
         else:
-            return False
+            print(99)
+            return 99
+    def Inisialitation(self, scan):
+        print('This is inisialisation !!')
     # def Input(self): 
-    # def Output(self): 
+    def Output(self, scan): 
+        print('This is print')
     # def Condition(self): 
     # def Looping(self): 
     # def Stack(self): 
     # def Function(self): 
+    def Syntax_error(self):
+        print('syntax error!!')
     def Execution(self):
         self.scan = self.File.readline().strip()
         while self.scan:
             self.temp = []
             # Check Function
-            if self.Inisialitation(self.scan) is True:
-                continue
-
+            s = self.Check_line(self.scan)
+            Function = {
+                1: self.Inisialitation(self.scan),
+                3: self.Output(self.scan)
+            }
+            Function.get(s, lambda: self.Syntax_error)
             self.scan = self.File.readline().strip()
         self.File.close()
 
